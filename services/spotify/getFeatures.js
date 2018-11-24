fd24a7330e85769503cb839c4777780b8b47174d
Anshori <ans4175@gmail.com>
2018-11-25 02:16:33 +0700

    chore(spotify): remove unnecessary comment

--- services/spotify/getFeatures.js
+++ services/spotify/getFeatures.js
@@ -1,4 +1,6 @@
 const SpotifyWebApi = require('spotify-web-api-node');
+
+// using my very own account app (ans4175)
 const spotifyApi = new SpotifyWebApi({
   clientId: 'a5f3224f62bd482c8f5d6217d77b8f60',
   clientSecret: '366b68297f624a43b2a13fbe95d193db'
@@ -11,8 +13,6 @@ module.exports = (trackArtist, trackName) => {
       .then(function(data) {
         // Set the access token on the API object so that it's used in all future requests
         spotifyApi.setAccessToken(data.body['access_token']);
-
-        // Get the most popular tracks by David Bowie in Great Britain
         return spotifyApi.searchTracks(`track:${trackName} artist:${trackArtist}`)
       })
       .then(function(data) {
